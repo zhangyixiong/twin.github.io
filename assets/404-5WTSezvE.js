38c2c4c6def167bdebc16c435c9893c97780c6e0
小张 <93377412+zhangyixiong@users.noreply.github.com>
2024-05-11 16:22:06 +0800

    Delete assets directory

--- assets/404-5WTSezvE.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index-ylGJ6s-O.js";const e={name:"404"};function r(n,o,s,_,c,a){return"404"}const f=t(e,[["render",r]]);export{f as default};
--- assets/index-PxSz8OpT.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./index-rMa9Cxep.js";import{_ as n,r as s,o as c,c as t}from"./index-ylGJ6s-O.js";const a={name:"Home",components:{BaseScene:o}};function r(p,_,m,f,i,B){const e=s("BaseScene");return c(),t(e)}const u=n(a,[["render",r]]);export{u as default};
