797a236fb16504ef1213d1719440dd69c153dbb2
小张 <93377412+zhangyixiong@users.noreply.github.com>
2024-05-11 16:01:15 +0800

    init

--- assets/index-jTIM_nGe.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./index-dyVg8fFD.js";import{_ as n,r as s,o as c,c as t}from"./index-xnB9RBpW.js";const a={name:"Home",components:{BaseScene:o}};function r(p,_,m,f,i,B){const e=s("BaseScene");return c(),t(e)}const u=n(a,[["render",r]]);export{u as default};
