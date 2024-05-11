2fcf0aa517bf6df1ea3b63a52a5b8868f6ffe4d7
小张 <93377412+zhangyixiong@users.noreply.github.com>
2024-05-11 16:17:37 +0800

    updata-[打包路径更改]

---
+++ assets/index-PxSz8OpT.js
@@ -0,0 +1,1 @@
+import{B as o}from"./index-rMa9Cxep.js";import{_ as n,r as s,o as c,c as t}from"./index-ylGJ6s-O.js";const a={name:"Home",components:{BaseScene:o}};function r(p,_,m,f,i,B){const e=s("BaseScene");return c(),t(e)}const u=n(a,[["render",r]]);export{u as default};
