d388af846a0e001b8f72802f79bcdbbd9c87387d
小张 <93377412+zhangyixiong@users.noreply.github.com>
2024-05-11 16:22:37 +0800

    updata-[打包文件更新]

---
+++ assets/index-h41f6Z4d.js
@@ -0,0 +1,1 @@
+import{B as o}from"./index-2qzj8-qR.js";import{_ as n,r as s,o as c,c as t}from"./index-4Ksvml6Z.js";const a={name:"Home",components:{BaseScene:o}};function r(p,_,m,f,i,B){const e=s("BaseScene");return c(),t(e)}const u=n(a,[["render",r]]);export{u as default};
