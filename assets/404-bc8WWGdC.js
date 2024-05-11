e4a664d166462efd4f75432a4155c21dcad6c9c9
小张 <93377412+zhangyixiong@users.noreply.github.com>
2024-05-11 17:01:39 +0800

    updata-[更新打包文件]

---
+++ assets/404-bc8WWGdC.js
@@ -0,0 +1,1 @@
+import{_ as e,o,e as t}from"./index-ATyGUcIh.js";const s={name:"404"},c={style:{color:"red"}};function r(n,_,a,p,d,f){return o(),t("div",c,"404")}const l=e(s,[["render",r]]);export{l as default};
---
+++ assets/index-nHM75VPp.js
@@ -0,0 +1,1 @@
+import{B as o}from"./index-0X74o5Wf.js";import{_ as n,r as s,o as c,c as t}from"./index-ATyGUcIh.js";const a={name:"Home",components:{BaseScene:o}};function r(p,_,m,f,i,B){const e=s("BaseScene");return c(),t(e)}const u=n(a,[["render",r]]);export{u as default};
