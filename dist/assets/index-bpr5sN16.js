3527e7a69353f272a692dd1d9a1c54c135b80714
小张 <93377412+zhangyixiong@users.noreply.github.com>
2024-05-17 10:22:46 +0800

    init-[初始化(打包文件与node服务器)]

---
+++ dist/assets/index-bpr5sN16.js
@@ -0,0 +1,1 @@
+import{B as o}from"./index-CRXmVjCW.js";import{_ as n,r as s,o as c,c as t}from"./index-w2vajOzX.js";const a={name:"Home",components:{BaseScene:o}};function r(p,_,m,f,i,B){const e=s("BaseScene");return c(),t(e)}const u=n(a,[["render",r]]);export{u as default};
