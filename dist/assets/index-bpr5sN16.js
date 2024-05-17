9a547d26857896bd2cbc5053278c0c54a85f73c9
小张 <93377412+zhangyixiong@users.noreply.github.com>
2024-05-17 10:32:47 +0800

    Delete dist directory

--- dist/assets/index-bpr5sN16.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./index-CRXmVjCW.js";import{_ as n,r as s,o as c,c as t}from"./index-w2vajOzX.js";const a={name:"Home",components:{BaseScene:o}};function r(p,_,m,f,i,B){const e=s("BaseScene");return c(),t(e)}const u=n(a,[["render",r]]);export{u as default};
